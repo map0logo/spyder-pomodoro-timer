73dbc2c010274613357d6d8d2e4d1428dc030c77
Francisco «mapologo» Palm <fpalm@qu4nt.com>
2021-12-01 23:46:47 -0400

    Add SpyderPomodoroTimerContainer

--- spyder-pomodoro-timer/spyder_pomodoro_timer/spyder/container.py
+++ spyder-pomodoro-timer/spyder_pomodoro_timer/spyder/container.py
@@ -12,6 +12,9 @@ from spyder.api.config.decorators import on_conf_change
 from spyder.api.translations import get_translation
 from spyder.api.widgets.main_container import PluginMainContainer
 
+# Local imports
+from spyder_pomodoro_timer.spyder.widgets import PomodoroTimerStatus
+
 _ = get_translation("spyder_pomodoro_timer.spyder")
 
 
@@ -22,7 +25,8 @@ class SpyderPomodoroTimerContainer(PluginMainContainer):
     # --- PluginMainContainer API
     # ------------------------------------------------------------------------
     def setup(self):
-        pass
+        # Widgets
+        self.pomodoro_timer_status = PomodoroTimerStatus(self)
 
     def update_actions(self):
         pass
