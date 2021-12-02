c7b5cc6c4ce3c4afcd3cb9d3474bdabe2b81e060
Francisco «mapologo» Palm <fpalm@qu4nt.com>
2021-12-01 23:45:52 -0400

    Add PomodoroTimerStatus

--- spyder-pomodoro-timer/spyder_pomodoro_timer/spyder/widgets.py
+++ spyder-pomodoro-timer/spyder_pomodoro_timer/spyder/widgets.py
@@ -8,16 +8,45 @@
 Spyder Pomodoro Timer Main Widget.
 """
 
-
+# Third party imports
+import qtawesome as qta
 
 # Spyder imports
 from spyder.api.config.decorators import on_conf_change
 from spyder.api.translations import get_translation
 
-from spyder.api.widgets.mixins import SpyderWidgetMixin
+from spyder.api.widgets.status import BaseTimerStatus
+from spyder.utils.icon_manager import ima
 
 
 # Localization
 _ = get_translation("spyder_pomodoro_timer.spyder")
 
 
+# ---- Widgets
+# ----------------------------------------------------------------------------
+
+
+class PomodoroTimerStatus(BaseTimerStatus):
+    """Status bar widget to display the pomodoro timer"""
+
+    ID = "pomodoro_timer_status"
+    CONF_SECTION = "spyder_pomodoro_timer"
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.value = "25:00"
+
+    def get_tooltip(self):
+        """Override api method."""
+
+        return "I am the Pomodoro timer!"
+
+    def get_icon(self):
+        return qta.icon("mdi.av-timer", color=ima.MAIN_FG_COLOR)
+
+    # ---- BaseTimerStatus API
+    def get_value(self):
+        """Get current time of the timer"""
+
+        return self.value
